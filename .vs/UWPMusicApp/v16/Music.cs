b02bc243a43a611a3544bbb9a8ed69a1293c03f8
grmanjula <63128282+grmanjula@users.noreply.github.com>
2020-05-23 13:56:57 -0700

    Add files via upload

---
+++ .vs/UWPMusicApp/v16/Music.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPMusicApp.Model
+{
+     public enum MusicCategory
+        {
+            Brunos,
+            Demis,
+            Drakes,
+            Selenas
+        }
+    //
+        public class Music
+        {
+            public string Name { get; set; }
+            public MusicCategory Category { get; set; }
+            public string AudioFile { get; set; }
+            public string ImageFile { get; set; }
+            public string Artist { get; set; }
+            public string Album { get; set; }
+
+        public Music(string name, MusicCategory category, string album, string artist)
+            {
+                Name = name;
+                Category = category;
+                AudioFile = $"/Assets/Music/{category}/{name}.mp3";
+                ImageFile = $"/Assets/Images/{category}/{name}.png";
+                Album = album;
+                Artist = artist;
+            }
+        }
+    }
+
+    
+
---
+++ .vs/UWPMusicApp/v16/MusicManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPMusicApp.Model
+{
+   public static class MusicManager
+    {
+        public static void GetAllSongs(ObservableCollection<Music> songs)
+        {
+            var allSongs = getSongs();
+            songs.Clear();
+            allSongs.ForEach(Music => songs.Add(Music)); //Lambda Expression
+
+        }
+
+        //
+        public static void GetMusicByCategory(ObservableCollection<Music> Songs, MusicCategory category)
+        {
+            var allSongs = getSongs();
+            var FilteredSounds = allSongs.Where(music => music.Category == category).ToList();
+            Songs.Clear();
+            FilteredSounds.ForEach(music => Songs.Add(music));
+        }
+
+        private static List<Music> getSongs()
+        {
+            var songs = new List<Music>();
+            songs.Add(new Model.Music("Magic", MusicCategory.Brunos, "24K Magic", "Bruno Mars"));
+            songs.Add(new Model.Music("Uptown", MusicCategory.Brunos, "UpTownFunk", "Mark Ronson"));
+            songs.Add(new Model.Music("Confident", MusicCategory.Demis, "ConfidentR", "Demi Lovato"));
+            songs.Add(new Model.Music("Sorry", MusicCategory.Demis,"Souveniers", "Demis Rousso"));
+            songs.Add(new Model.Music("Hotline", MusicCategory.Drakes, "Hotline Bling","Drake Graham"));
+            songs.Add(new Model.Music("Scorpion", MusicCategory.Drakes, "Scorpion 2018","Drake Graham"));
+            songs.Add(new Model.Music("LoseU", MusicCategory.Selenas, "Lose You to Love Me" , "Selena Gomez"));
+            songs.Add(new Model.Music("StarsDance", MusicCategory.Selenas, "Stars Dance 2013", "Selena Gomez"));
+
+            return songs;
+        }
+
+    }
+}
