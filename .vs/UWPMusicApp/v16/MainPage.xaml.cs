e1c48d0e305177077755bba37351760759d99349
grmanjula <63128282+grmanjula@users.noreply.github.com>
2020-05-23 13:54:12 -0700

    Changes to Artist and Album Names

---
+++ .vs/UWPMusicApp/v16/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UWPMusicApp.Model;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPMusicApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    //
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Music> Songs;
+        //private ObservableCollection<MusicLib> MusicLibrary;
+        
+        private List<MenuItem> MenuItems;
+
+        //private ObservableCollection<MusicLib> MusicList = new ObservableCollection<MusicLib>();
+
+
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Songs = new ObservableCollection<Music>();
+            MusicManager.GetAllSongs(Songs);
+            MenuItems = new List<MenuItem>();
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/bruno.png", Category = MusicCategory.Brunos });
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/demi.png", Category = MusicCategory.Demis });
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/drake.png", Category = MusicCategory.Drakes });
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/selena.png", Category = MusicCategory.Selenas });
+
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+    
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void MenuItemsListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var menuItem = (MenuItem)e.ClickedItem;
+            CategoryTextBlock.Text = menuItem.Category.ToString();
+            MusicManager.GetMusicByCategory(Songs, menuItem.Category);
+            BackButton.Visibility = Visibility.Visible;
+
+        }
+
+        private void SoundGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var music = (Music)e.ClickedItem;
+            MyMediaElement.Source = MediaSource.CreateFromUri(new Uri(BaseUri, music.AudioFile));
+            ArtistTextBlock.Text = music.Artist;
+            AlbumTextBlock.Text = music.Album;
+           
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            MusicManager.GetAllSongs(Songs);
+            CategoryTextBlock.Text = "All Music";
+            MenuItemsListView.SelectedItem = null;
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+    }
+
+  }
+
+
